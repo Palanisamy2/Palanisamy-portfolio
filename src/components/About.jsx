84c2c3294b5b4849bb5d5c232e8db6f2935a944b
Palanisamy M <146609846+Palanisamy2@users.noreply.github.com>
2025-07-03 10:09:20 +0530

    Update profile image in Hero and About components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -276,7 +276,7 @@ const About = () => {
                   className="w-48 h-48 mx-auto rounded-full overflow-hidden border-4 border-white dark:border-gray-700 shadow-xl"
                 >
                   <img
-                    src="/palanisamy-img.png"
+                    src="/palanisamy-img copy.png"
                     alt="Palanisamy M"
                     className="w-full h-full object-cover"
                   />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -132,7 +132,7 @@ const Hero = () => {
                 className="relative w-80 h-80 rounded-full overflow-hidden border-4 border-white dark:border-gray-700 shadow-2xl"
               >
                 <img
-                  src="/palanisamy-img.png"
+                  src="/palanisamy-img copy.png"
                   alt="Palanisamy M"
                   className="w-full h-full object-cover"
                 />
